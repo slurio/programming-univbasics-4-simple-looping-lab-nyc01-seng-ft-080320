24a8c494cca50470a4c4f0df17249e05a3b8ac8e
Samantha Lurio <samantha.lurio@gmail.com>
2020-07-07 17:56:22 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -27,9 +27,10 @@ end
 
 def return_string_array(array)
   counter = 0
+  new_array = []
   while array[counter] do
-    array[counter] = array[counter].to_s
+    new_array.push(array[counter].to_s)
     counter += 1
   end
-  array
+  new_array
 end
